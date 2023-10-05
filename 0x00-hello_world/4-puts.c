26341108b18f2341b803d29e3f752b43272d1c83
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-05 09:29:09 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *Description > this is a prog prints  starts with \" by puts fun
+ *
+ * Return: Always 0 means (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
