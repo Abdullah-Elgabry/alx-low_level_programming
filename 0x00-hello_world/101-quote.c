3ea6035e3ca58da32e4d769e536259a061c8df89
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-05 10:30:26 -0700

    advanced print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ *Description > use write func
+ *
+ * to print this sen
+ *
+ * Return: 1 means (not success)
+*/
+
+int main(void)
+{	
+	char doubqo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, doubqo, (sizeof(quo) / sizeof(char)));
+	return (1);
+}
