a5d799b6fada5cb27d44e999b543f154635a9056
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-07 05:07:29 -0700

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Description > print alphapet in lowercase
+ *
+ * Return: Always 0 means (success)
+*/
+
+int main(void)
+{	char c = 'a';
+	while (c <= 'z')
+	{	putchar(c"\n");
+		c++;
+	}
+	return (0);
+}
