550cd01c7953118fe7be4bdc6c476a0ced60c1cf
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-07 06:28:56 -0700

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Description > print alphapet in lowercase
+ *
+ * Return: Always 0 means (success)
+*/
+
+int main(void)
+{	int start = 48;
+
+	while (start <= 102)
+	{
+		putchar(start);
+		if (start == 57
+			satrt += 39;
+		start++;
+	}
+	putchar('\n');
+
+	return (0);
+}
