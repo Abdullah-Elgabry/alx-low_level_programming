54c60a07eeae36d4dbc2e7e94aadbad030c70891
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-07 06:39:47 -0700

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Description > prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 means (success)
+*/
+
+int main(void)
+{	int start = 0;
+
+	while (start <= 9)
+	{
+		putchar(start + 48);
+		if (start != 9)
+			putchar(' ,');
+		start++;
+	}
+	putchar('\n');
+
+	return (0);
+}
