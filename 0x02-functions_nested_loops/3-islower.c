e54a91d0bdab0faf2dad28f1a25c4ec2ca5074e4
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-10 12:46:54 -0700

    Write a function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main."
+
+/**
+ * _islower -  this fun check for lowercase
+ *
+ * @ch: input of fun
+ *
+ * Return: On success 1 else print 0 success
+*/
+
+int _islower(int ch)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
