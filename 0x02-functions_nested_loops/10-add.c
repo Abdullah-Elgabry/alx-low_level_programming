d0c355f88588ae93f71cd2fdd6540406454f7be2
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-10 13:59:38 -0700

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha  -  this fun adds 2 int
+ *
+ * @ch: input of fun
+ *
+ * Return: sum of a and b
+*/
+
+int _isalpha(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
