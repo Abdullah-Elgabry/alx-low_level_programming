33d466ab03d79f48054d767c0a45e21f00fbb028
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-16 12:48:52 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates its value
+ *
+ * @ptr: parameter
+ *
+ * return: nothing
+*/
+
+void reset_to_98 (int *ptr)
+{
+	*ptr = 98;
+}
