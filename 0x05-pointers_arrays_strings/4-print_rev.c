e65f6fb1e4870aeb423153739edf6a0f6371d231
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-16 14:06:45 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev  - prints a string, in reverse, followed by a new line.
+ *
+ * @rev: this is the ptr to str will be printed
+ *
+ * Return: void
+*/
+
+void print_rev(char *rev)
+{
+	int c = 0;
+
+	while (rev[c])
+		c++;
+
+	while (c--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
