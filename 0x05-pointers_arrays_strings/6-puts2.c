ad451440b9965b19e56c8f048b8974af53beb2bf
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-16 14:33:37 -0700

    prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints str + /n
+ *
+ * @s: this is the ptr to str will be printed
+ *
+ * Return: Nothing
+*/
+
+void puts2(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; ++i)
+	{
+		if (n % 2 == 0)
+			_putchar(s[n]);
+	}
+	_putchar('\n');
+}
