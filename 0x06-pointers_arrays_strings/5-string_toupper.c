d354c38a7a9ed26ec8576ce0a4c466ac1ee51150
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-19 11:23:20 -0700

    5. Always look up (lower case to upper case)

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - fun that changes lowercase to uppercase.
+ *
+ * @s: this is the returned string
+ *
+ * Return: converted char
+*/
+
+char *string_toupper(char *s)
+{
+	int start;
+
+	for (start = 0; s[start] != '\0'; start++)
+	{
+		if (s[start] >= 97 && s[start] <= 122)
+		{
+			s[start] = s[start] - 32;
+		}
+	}
+	return (s)
+}
