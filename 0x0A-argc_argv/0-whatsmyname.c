0fa64e112e2409960fe944cf2f26827ff7031108
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-27 11:24:29 -0700

    0. program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints its name, followed by a new line
+  * @argc: counter
+  * @argv: char list of value
+  *
+  * Return:czero
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
