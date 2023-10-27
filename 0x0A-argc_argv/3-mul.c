e485a61ef504e16361fcd998a7ec777566631712
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-27 11:41:32 -0700

    1. program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,13 @@
   *
   * Return: 0
   */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int cont;
 
 	if (argc == 3)
 	{
-		cont = atoi(argv[1] * atoi(argv[2]);
+		cont = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", cont);
 	}
 	else
