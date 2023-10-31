b949cc1e374fe45a34c72cdc38268a7cae475d6a
“abdullah <abdullah.h.elgabry@gmail.com>
2023-10-31 10:10:09 -0700

    function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * argstostr - fun that concatenates all the arguments of your program.
  * @ac: integer that counts the arguments
@@ -9,34 +8,34 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, c, s;
-    char *str;
+	int s, j, c = 0, size = 0;
+	char *str;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            ;
-        s += j;
-    }
+	for (s = 0; s < ac; s++)
+	{
+		for (j = 0; av[s][j]; j++)
+			size++;
+	}
+	size += ac;
 
-    str = malloc(s + 1);
-    if (str == NULL)
-        return (NULL);
+	str = malloc(sizeof(char) * size + 1);
+	if (str == NULL)
+		return (NULL);
 
-    c = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            str[c] = av[i][j];
-            c++;
-        }
-        str[c] = '\0';
-        c++;
-    }
-
-    return (str);
+	for (s = 0; s < ac; s++)
+	{
+		for (j = 0; av[s][j]; j++)
+		{
+			str[c] = av[s][j];
+			c++;
+		}
+		if (str[c] == '\0')
+		{
+			str[c++] = '\n';
+		}
+	}
+	return (str);
 }
