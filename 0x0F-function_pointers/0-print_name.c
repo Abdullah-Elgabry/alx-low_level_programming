d2a5a3d0eb5edb6283724083de68b9926f756788
“abdullah <abdullah.h.elgabry@gmail.com>
2023-11-08 15:03:50 -0800

    0.  function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+* print_name - fun will print the name
+*
+*@name: the name will be printed
+
+*@f: print fun ptr
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
