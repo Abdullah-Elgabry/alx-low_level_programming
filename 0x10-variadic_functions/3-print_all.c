1697a2deb0864feb7f61e1c12b3d216f0943d43d
“abdullah <abdullah.h.elgabry@gmail.com>
2023-11-09 17:46:50 -0800

    3. function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,37 +13,37 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	int n = 0, q = 0;
-	char *sep = ", ";
+	int n = 0, i = 0;
+	char *term = ", ";
 	char *newl;
 
 	va_start(valist, format);
 
-	while (format && format[q])
-		q++;
+	while (format && format[i])
+		i++;
 
 	while (format && format[n])
 	{
-		if (n  == (q - 1))
+		if (n  == (i - 1))
 		{
-			sep = "";
+			term = "";
 		}
 		switch (format[n])
 		{
 		case 'c':
-			printf("%c%s", va_arg(valist, int), sep);
+			printf("%c%s", va_arg(valist, int), term);
 			break;
-		case 'q':
-			printf("%d%s", va_arg(valist, int), sep);
+		case 'i':
+			printf("%d%s", va_arg(valist, int), term);
 			break;
 		case 'f':
-			printf("%f%s", va_arg(valist, double), sep);
+			printf("%f%s", va_arg(valist, double), term);
 			break;
 		case 's':
 			newl = va_arg(valist, char *);
 			if (newl == NULL)
 				newl = "(nil)";
-			printf("%s%s", newl, sep);
+			printf("%s%s", newl, term);
 			break;
 		}
 		n++;
