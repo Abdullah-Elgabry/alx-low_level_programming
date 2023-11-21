57be8275b33a7213d3886ea064fcca6d819422d0
“abdullah <abdullah.h.elgabry@gmail.com>
2023-11-21 05:19:03 -0800

    del the head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint -  deletes the 1st node of a listint_t.
+ *
+ * @head: ptr of 1st node address.
+ *
+ * Return: value of deleted node.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *_temp_node;
+	int t;
+
+	if (!head)
+		return (0);
+	_temp_node = (*head)->next;
+	t = (*head)->t;
+	free(*head);
+	*head = _temp_node;
+	return (t);
+}
