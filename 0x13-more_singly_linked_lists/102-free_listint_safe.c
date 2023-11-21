3e9149b51f125f2a03e129819cfdbb7a31d53b14
“abdullah <abdullah.h.elgabry@gmail.com>
2023-11-21 07:37:49 -0800

    adv+ frees a listint_t list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,42 +1,71 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint_safe - this func will frees a linkedList.
+ * searcher - this func will seaech for a search_L in a linked list
  *
- * @h: 1st node ptr.
+ * @head: this is the list will be search for
  *
- * Return: list size
+ * Return: wiil return node add
  */
+
+
+
+listint_t *searcher(listint_t *head)
+{
+	listint_t *bottom, *pointer;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (bottom = head->next; bottom != NULL; bottom = bottom->next)
+	{
+		if (bottom == bottom->next)
+			return (bottom);
+		for (pointer = head; pointer != bottom; pointer = pointer->next)
+			if (pointer == bottom->next)
+				return (bottom->next);
+	}
+	return (NULL);
+}
+
+/**
+ * free_listint_safe - this func will frees the Llist
+ *
+ * @h: list header
+ *
+ * Return: free nodes num
+ */
+
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t siz = 0;
-	int disc;
-	listint_t *_node;
+	listint_t *next, *_n;
+	size_t siz;
+	int search_L = 1;
 
-	if (!h || !*h)
+	if (h == NULL || *h == NULL)
 		return (0);
 
-	while (*h)
+	_n = searcher(*h);
+	for (siz = 0; (*h != _n || search_L) && *h != NULL; *h = next)
 	{
-		disc = *h - (*h)->next;
-		if (disc > 0)
+		siz++;
+		next = (*h)->next;
+		if (*h == _n && search_L)
 		{
-			_node = (*h)->next;
-			free(*h);
-			*h = _node;
+			if (_n == _n->next)
+			{
+				free(*h);
+				break;
+			}
 			siz++;
+			next = next->next;
+			free((*h)->next);
+			search_L = 0;
 		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			siz++;
-			break;
-		}
+		free(*h);
 	}
-
 	*h = NULL;
-
 	return (siz);
 }
