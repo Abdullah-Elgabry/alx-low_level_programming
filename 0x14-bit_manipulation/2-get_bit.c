8e94dd3b42c2750f40b8d024d1438c8e6ef833e2
“abdullah <abdullah.h.elgabry@gmail.com>
2023-11-23 11:23:01 -0800

    init

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - this func will returns the value of a bit at a given index.
+ *
+ * @n: indexed num.
+ *
+ * @index: given bit
+ *
+ * Return: vit value 
+ * otherwise : (-1)
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (n >> index & 1);
+}
