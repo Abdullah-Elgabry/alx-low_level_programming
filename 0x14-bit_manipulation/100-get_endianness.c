4af9324f4e7507c1e21f3a704b4590ac494b8b86
“abdullah <abdullah.h.elgabry@gmail.com>
2023-11-23 11:40:20 -0800

    init

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - this func will checks the endianness.
+ *
+ * Return: 0 if big endian,
+ * 1 if little endian
+ */
+
+
+int get_endianness(void)
+{
+	unsigned long int i = 1;
+
+	return (*(char *)&i);
+}
